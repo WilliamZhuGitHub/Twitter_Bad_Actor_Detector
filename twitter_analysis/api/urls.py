5d0e92a7abfee0f53a8558ac3099fcbd56dfcc06
WilliamZhuGitHub <zhuw2@wit.edu>
2022-04-12 22:14:18 -0400

    Removed Unused files

--- twitter_analysis/api/urls.py
+++ twitter_analysis/api/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 #from twitter_analysis.api.models import Room
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, GetTwitterUser, GetUserTweets
+from .views import  GetTwitterUser, GetUserTweets
 
 urlpatterns = [
-    path('room-list', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view()),
-    path('get-room', GetRoom.as_view()),
-    path('join-room', JoinRoom.as_view()),
     path('get-twitter-user', GetTwitterUser.as_view()),
     path('get-user-tweets', GetUserTweets.as_view())
 ]
